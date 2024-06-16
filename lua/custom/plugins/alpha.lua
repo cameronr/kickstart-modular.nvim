e164bc6163c79466d82a0a83c763de8ee03b69f0
Cameron Ring <cameron@cs.stanford.edu>
2024-06-16 16:00:14 -0700

    add dashboard

---
+++ lua/custom/plugins/alpha.lua
@@ -0,0 +1,40 @@
+return {
+  'goolord/alpha-nvim',
+  event = 'VimEnter',
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+    -- Set header
+    dashboard.section.header.val = {
+      '                                                     ',
+      '  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ',
+      '  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ',
+      '  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ',
+      '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
+      '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
+      '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
+      '                                                     ',
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  New File', '<cmd>ene<CR>'),
+      dashboard.button('SPC sf', '󰱼  Open Files'),
+      dashboard.button('SPC sg', '  Search files'),
+      dashboard.button('SPC s.', '󱑎  Recent Files'),
+      dashboard.button('SPC ng', '󰊢  Git'),
+      -- dashboard.button('SPC wr', '󰁯  Restore Session For Current Directory', '<cmd>SessionRestore<CR>'),
+      dashboard.button('u', '  Update plugins', '<cmd>Lazy sync<CR>'),
+      dashboard.button('t', '  Install language tools', '<cmd>Mason<CR>'),
+      dashboard.button('c', '  Edit configuration', ':e init.lua | :cd ~/.config/nvim<CR>'),
+      dashboard.button('q', '󰩈  Quit NVIM', '<cmd>qa<CR>'),
+    }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
+  end,
+}
