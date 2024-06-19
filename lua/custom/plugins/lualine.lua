a8a97f036b338db28d41026441e76acb677c16bc
Cameron Ring <cameron@cs.stanford.edu>
2024-06-18 17:53:34 -0700

    shrink lualine when window gets smaller

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -13,9 +13,27 @@ return {
     custom_tokyonight.visual.b.bg = '#1f2335'
     custom_tokyonight.replace.b.bg = '#1f2335'
 
+    --- From: https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets
+    --- @param trunc_width number trunctates component when screen width is less then trunc_width
+    --- @param trunc_len number truncates component to trunc_len number of chars
+    --- @param hide_width number hides component when window width is smaller then hide_width
+    --- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+    --- return function that can format the component accordingly
+    local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
+      return function(str)
+        local win_width = vim.fn.winwidth(0)
+        if hide_width and win_width < hide_width then
+          return ''
+        elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
+          return str:sub(1, trunc_len) .. (no_ellipsis and '' or '...')
+        end
+        return str
+      end
+    end
+
     -- Show LSP status, borrowed from Heirline cookbook
     -- https://github.com/rebelot/heirline.nvim/blob/master/cookbook.md#lsp
-    local function lualine_lsp_status()
+    local function lsp_status_all()
       local haveServers = false
       local names = {}
       for _, server in pairs(vim.lsp.get_clients { bufnr = 0 }) do
@@ -29,15 +47,39 @@ return {
       return ' ' .. table.concat(names, ',')
     end
 
+    -- Override 'encoding': Don't display if encoding is UTF-8.
+    local encoding_only_if_not_utf8 = function()
+      local ret, _ = (vim.bo.fenc or vim.go.enc):gsub('^utf%-8$', '')
+      return ret
+    end
+    -- fileformat: Don't display if &ff is unix.
+    local fileformat_only_if_not_unix = function()
+      local ret, _ = vim.bo.fileformat:gsub('^unix$', '')
+      return ret
+    end
+
     local opts = {
       options = {
         theme = custom_tokyonight,
+        -- globalstatus = true,
       },
       sections = {
-        lualine_a = { 'mode' },
+        lualine_a = {
+          {
+            'mode',
+            fmt = trunc(90, 4, 0, true),
+          },
+        },
         lualine_b = {
-          'branch',
-          'diff',
+          {
+            'branch',
+            fmt = trunc(70, 15, 65, true),
+          },
+
+          {
+            'diff',
+            fmt = trunc(0, 0, 60, true),
+          },
           {
             'diagnostics',
             -- symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
@@ -52,14 +94,31 @@ return {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = '#ff9e64' },
+            fmt = trunc(0, 0, 100, true), -- hide when window is < 100 columns
           },
-          lualine_lsp_status,
-          'encoding',
-          'fileformat',
-          'filetype',
+          {
+            lsp_status_all,
+            fmt = trunc(0, 0, 90, true),
+          },
+          {
+            encoding_only_if_not_utf8,
+            fmt = trunc(0, 0, 80, true), -- hide when window is < 80 columns
+          },
+          {
+            fileformat_only_if_not_unix,
+            fmt = trunc(0, 0, 80, true), -- hide when window is < 80 columns
+          },
+          {
+            'filetype',
+            fmt = trunc(0, 0, 100, true), -- hide when window is < 100 columns
+          },
         },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
+        lualine_y = {
+          { 'progress', fmt = trunc(0, 0, 40, true) },
+        },
+        lualine_z = {
+          { 'location', fmt = trunc(0, 0, 80, true) },
+        },
       },
       extensions = {
         'fzf',
