e81204cbb1d20632ac3fabf067a079bb5951a831
Cameron Ring <cameron@cs.stanford.edu>
2024-06-22 14:03:08 -0700

    don't use arrows to nav cmp popup

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -78,6 +78,16 @@ return {
           ['<Tab>'] = cmp.mapping.select_next_item(),
           ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
+          -- Don't use up/down for the selection window
+          ['<Down>'] = cmp.mapping(function(fallback)
+            cmp.close()
+            fallback()
+          end, { 'i' }),
+          ['<Up>'] = cmp.mapping(function(fallback)
+            cmp.close()
+            fallback()
+          end, { 'i' }),
+
           -- Backslash to close the popup
           ['<Bslash>'] = cmp.mapping.close(),
 
@@ -138,10 +148,10 @@ return {
       }
 
       -- https://github.com/hrsh7th/nvim-cmp/issues/181
-      local has_words_before = function()
-        local cursor = vim.api.nvim_win_get_cursor(0)
-        return (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ''):sub(cursor[2], cursor[2]):match '%s'
-      end
+      -- local has_words_before = function()
+      --   local cursor = vim.api.nvim_win_get_cursor(0)
+      --   return (vim.api.nvim_buf_get_lines(0, cursor[1] - 1, cursor[1], true)[1] or ''):sub(cursor[2], cursor[2]):match '%s'
+      -- end
 
       -- Enable command-line completion
 
@@ -165,28 +175,28 @@ return {
               fallback()
             end
           end, { 'c' }),
-          ['<Down>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-              -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-              -- that way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { 'c' }),
-          ['<Up>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'c' }),
+          -- ['<Down>'] = cmp.mapping(function(fallback)
+          --   if cmp.visible() then
+          --     cmp.select_next_item()
+          --     -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          --     -- that way you will only jump inside the snippet region
+          --   elseif luasnip.expand_or_jumpable() then
+          --     luasnip.expand_or_jump()
+          --   elseif has_words_before() then
+          --     cmp.complete()
+          --   else
+          --     fallback()
+          --   end
+          -- end, { 'c' }),
+          -- ['<Up>'] = cmp.mapping(function(fallback)
+          --   if cmp.visible() then
+          --     cmp.select_prev_item()
+          --   elseif luasnip.jumpable(-1) then
+          --     luasnip.jump(-1)
+          --   else
+          --     fallback()
+          --   end
+          -- end, { 'c' }),
         },
         sources = {
           { name = 'cmdline' },
