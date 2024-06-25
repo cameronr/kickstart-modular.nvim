087cf6eb44ded18a840dccef906824c92c780581
Cameron Ring <cameron@cs.stanford.edu>
2024-06-24 20:16:53 -0700

    support no autoinstall flag for linters

--- lua/custom/plugins/mason-nvim-lint.lua
+++ lua/custom/plugins/mason-nvim-lint.lua
@@ -3,9 +3,9 @@ return {
     'rshkarin/mason-nvim-lint',
     event = 'VeryLazy',
     config = function()
-      require('mason-nvim-lint').setup {
-        automatic_installation = true,
-      }
+      require('mason-nvim-lint').setup({
+        automatic_installation = not vim.g.no_mason_autoinstall,
+      })
     end,
   },
 }
