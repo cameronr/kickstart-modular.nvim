62032a7b1eab83688733ccb972c71b54be0b7227
Cameron Ring <cameron@cs.stanford.edu>
2024-07-08 21:50:14 -0700

    Add ftplugin for lua keymaps

---
+++ ftplugin/lua.lua
@@ -0,0 +1,3 @@
+-- Testing
+vim.keymap.set('n', '<leader>tf', '<cmd>PlenaryBustedFile %<CR>', { desc = 'Test file' })
+vim.keymap.set('n', '<leader>td', '<cmd>PlenaryBustedFile %<CR>', { desc = 'Test directory' })
