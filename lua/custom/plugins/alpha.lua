3055ee8cce8ce2a0e152dd83e01fc0b6caf353fb
Cameron Ring <cameron@cs.stanford.edu>
2024-07-17 00:33:45 -0700

    feat: ver on dashboard, plugin stats

--- lua/custom/plugins/alpha.lua
+++ lua/custom/plugins/alpha.lua
@@ -4,6 +4,7 @@ return {
   config = function()
     local alpha = require('alpha')
     local dashboard = require('alpha.themes.dashboard')
+    local version = vim.version()
 
     -- Set header
     dashboard.section.header.val = {
@@ -14,26 +15,46 @@ return {
       '  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ',
       '  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ',
       '  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ',
-      '                                                     ',
+      '                                              ' .. version.major .. '.' .. version.minor .. '.' .. version.patch,
     }
 
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button('e', '  New File', '<cmd>ene<CR>'),
-      dashboard.button('SPC sf', '󰱼  Open Files'),
-      dashboard.button('SPC sg', '  Search files'),
-      dashboard.button('SPC s.', '󱑎  Recent Files'),
+      dashboard.button('f', '󰱼  Open Files', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('g', '  Greg in Files', '<cmd>Telescope live_grep<CR>'),
+      dashboard.button('.', '󱑎  Recent Files', '<cmd>Telescope oldfiles <CR>'),
       dashboard.button('SPC n', '󰊢  Neogit'),
       dashboard.button('r', '󰁯  Restore a Session', "<cmd>lua require('auto-session.session-lens').search_session()<CR>"),
       -- dashboard.button('c', '󰁯  Restore Session For Current Directory', '<cmd>SessionRestore<CR>'),
       dashboard.button('l', '󰒲  Lazy', '<cmd>Lazy<CR>'),
       dashboard.button('m', '  Mason', '<cmd>Mason<CR>'),
-      dashboard.button('c', '  Neovim configuration', '<cmd>cd ~/.config/nvim<CR> | <cmd>SessionRestore <CR>'),
+      -- dashboard.button('c', '  Neovim configuration', '<cmd>cd ~/.config/nvim<CR> | <cmd>SessionRestore <CR>'),
       dashboard.button('q', '󰩈  Quit NVIM', '<cmd>qa<CR>'),
     }
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = 'AlphaButtons'
+      button.opts.hl_shortcut = 'AlphaShortcut'
+    end
+    dashboard.section.header.opts.hl = 'AlphaHeader'
+    dashboard.section.buttons.opts.hl = 'AlphaButtons'
+    dashboard.section.footer.opts.hl = 'AlphaFooter'
+    -- dashboard.opts.layout[1].val = 8
 
     -- Send config to alpha
     alpha.setup(dashboard.opts)
+
+    -- Borrowed from LazyVim
+    vim.api.nvim_create_autocmd('User', {
+      once = true,
+      pattern = 'LazyVimStarted',
+      callback = function()
+        local stats = require('lazy').stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
 
     -- Disable folding on alpha buffer
     vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
