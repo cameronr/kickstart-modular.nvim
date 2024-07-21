09bb3acf9f30ac7b5a5ab95811a3b5616b052643
Cameron Ring <cameron@cs.stanford.edu>
2024-07-20 23:30:12 -0700

    fix: telescope hot key for fuzzy search

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -168,7 +168,7 @@ return {
       vim.keymap.set('n', '<leader>s"', '<cmd>Telescope neoclip<CR>', { desc = 'Yanks' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>sF', function()
+      vim.keymap.set('n', '<leader>sz', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
           winblend = 10,
