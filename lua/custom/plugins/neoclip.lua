7810235eb8cf7d55435fac35c6471af89aacb70f
Cameron Ring <cameron@cs.stanford.edu>
2024-08-22 23:43:40 -0700

    fix: clean up neoclip config

--- lua/custom/plugins/neoclip.lua
+++ lua/custom/plugins/neoclip.lua
@@ -1,9 +1,10 @@
 return {
   'AckslD/nvim-neoclip.lua',
+  event = 'VeryLazy',
   dependencies = {
     -- you'll need at least one of these
     { 'nvim-telescope/telescope.nvim' },
     -- {'ibhagwan/fzf-lua'},
   },
-  config = function() require('neoclip').setup() end,
+  opts = {},
 }
