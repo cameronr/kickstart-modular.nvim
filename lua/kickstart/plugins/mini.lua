4f6102a953c41a0ad099df55cdaafd2536457ab8
Cameron Ring <cameron@cs.stanford.edu>
2024-09-09 22:58:33 -0700

    fix(mini): lazy load

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -1,6 +1,7 @@
 return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
+    event = 'VeryLazy',
 
     -- Mock web-dev-icons with mini-icons
     init = function()
