0ac0a7e62d4bc66c630280c25ca472c8650f8424
Cameron Ring <cameron@cs.stanford.edu>
2024-09-09 22:57:52 -0700

    fix(nvim-lint): lazy load

--- lua/custom/plugins/mason-nvim-lint.lua
+++ lua/custom/plugins/mason-nvim-lint.lua
@@ -1,7 +1,7 @@
 return {
   {
     'rshkarin/mason-nvim-lint',
-    event = 'VeryLazy',
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       require('mason-nvim-lint').setup({
         automatic_installation = not vim.g.no_mason_autoinstall,
