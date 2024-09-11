b77936d4d3de947cfe1f2ca314f4ae215ce56085
Cameron Ring <cameron@cs.stanford.edu>
2024-09-10 23:15:13 -0700

    fix(diffview): no underline in diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -2,10 +2,18 @@ return {
   'sindrets/diffview.nvim',
   -- lazy = true,
   keys = {
-    { '<leader>hd', '<cmd>DiffviewOpen<CR>', desc = 'git [d]iff against index' },
+    { '<leader>hd', '<cmd>DiffviewOpen<CR>', desc = 'git diff against index' },
   },
   cmd = 'DiffviewOpen',
   opts = {
     enhanced_diff_hl = true,
+    hooks = {
+      diff_buf_win_enter = function(bufnr, winid, ctx)
+        -- Turn off cursor line for diffview windows because of BG conflict
+        -- setting gross underline:
+        -- https://github.com/neovim/neovim/issues/9800
+        vim.wo[winid].culopt = 'number'
+      end,
+    },
   },
 }
