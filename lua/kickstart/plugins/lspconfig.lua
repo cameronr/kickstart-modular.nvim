e2e21a79603eb4f206076861cff4731d9063f31f
Cameron Ring <cameron@cs.stanford.edu>
2024-09-10 23:15:40 -0700

    fix(lspconfig): twean inlay hint keymap: vH

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -156,7 +156,7 @@ return {
           --
           -- This may be unwanted, since they displace some of your code
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>vh', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })) end, 'Toggle inlay hints')
+            map('<leader>vH', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })) end, 'Toggle inlay hints')
           end
         end,
       })
