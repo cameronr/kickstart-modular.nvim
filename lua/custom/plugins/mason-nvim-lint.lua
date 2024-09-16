71cbfd2cf5a9058a7242b1fbdf3534ab850fe5a9
Cameron Ring <cameron@cs.stanford.edu>
2024-09-15 20:33:52 -0700

    fix(mason-nvim-lint): use opts

--- lua/custom/plugins/mason-nvim-lint.lua
+++ lua/custom/plugins/mason-nvim-lint.lua
@@ -2,10 +2,8 @@ return {
   {
     'rshkarin/mason-nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('mason-nvim-lint').setup({
-        automatic_installation = not vim.g.no_mason_autoinstall,
-      })
-    end,
+    opts = {
+      automatic_installation = not vim.g.no_mason_autoinstall,
+    },
   },
 }
