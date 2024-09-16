b2bf665de9176824747637efe7c4394989059d25
Cameron Ring <cameron@cs.stanford.edu>
2024-09-15 20:32:17 -0700

    fix(mason): split out into it's own config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -19,13 +19,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      {
-        'williamboman/mason.nvim',
-        config = true,
-        dependencies = {
-          'ptevearc/conform.nvim',
-        },
-      }, -- NOTE: Must be loaded before dependants
+      'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
