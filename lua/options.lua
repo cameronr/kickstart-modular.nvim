a5da4d9083940ac2849b362e7e3705a5baf6ef7e
Cameron Ring <cameron@cs.stanford.edu>
2024-09-24 11:02:19 -0700

    fix(options): turn off virtualedit

--- lua/options.lua
+++ lua/options.lua
@@ -195,6 +195,7 @@ vim.diagnostic.config({
 
 vim.opt.wrap = true
 
-vim.opt.virtualedit = 'block'
+-- Don't think I need since I can use $ / A when editing end of line in V-Block mode (x)
+-- vim.opt.virtualedit = 'block'
 
 -- vim: ts=2 sts=2 sw=2 et
