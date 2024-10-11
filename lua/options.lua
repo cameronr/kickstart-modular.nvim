06fc19e1df6a2aca1c4e7581260f92b6b91dbcee
Cameron Ring <cameron@cs.stanford.edu>
2024-10-11 09:48:32 -0700

    fix(options): use tighter cursor restore function

--- lua/options.lua
+++ lua/options.lua
@@ -149,20 +149,18 @@ end
 
 -- Both of these from https://www.reddit.com/r/neovim/comments/1abd2cq/what_are_your_favorite_tricks_using_neovim/
 -- Jump to last position when reopening a file
--- vim.api.nvim_create_autocmd('BufReadPost', {
---   desc = 'Open file at the last position it was edited earlier',
---   group = misc_augroup,
---   pattern = '*',
---   command = 'silent! normal! g`"zv',
--- })
-
 vim.api.nvim_create_autocmd('BufReadPost', {
   desc = 'Open file at the last position it was edited earlier',
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then vim.api.nvim_win_set_cursor(0, mark) end
-  end,
+  command = 'silent! normal! g`"zv',
 })
+
+-- vim.api.nvim_create_autocmd('BufReadPost', {
+--   desc = 'Open file at the last position it was edited earlier',
+--   callback = function()
+--     local mark = vim.api.nvim_buf_get_mark(0, '"')
+--     if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then vim.api.nvim_win_set_cursor(0, mark) end
+--   end,
+-- })
 
 -- Always open help on the right
 -- Open help window in a vertical split to the right.
