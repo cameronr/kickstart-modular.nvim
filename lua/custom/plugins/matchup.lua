47a0be2a390b7123ca96bcbfec98df44165d5843
Cameron Ring <cameron@cs.stanford.edu>
2024-10-13 11:45:00 -0700

    feat(matchup): add

---
+++ lua/custom/plugins/matchup.lua
@@ -0,0 +1,9 @@
+return {
+  'andymass/vim-matchup',
+  -- enabled = false,
+  event = { 'BufReadPost', 'BufNewFile' },
+  config = function()
+    -- may set any options here
+    vim.g.matchup_matchparen_offscreen = { method = 'popup' }
+  end,
+}
