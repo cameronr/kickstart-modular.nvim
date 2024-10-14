2779090d126bb794fffca9a7ed0fead9692af339
Cameron Ring <cameron@cs.stanford.edu>
2024-10-13 23:40:12 -0700

    feat(line-number-mode-change): add

---
+++ lua/custom/plugins/line-number-mode-change.lua
@@ -0,0 +1,44 @@
+return {
+  'cameronr/line-number-change-mode.nvim',
+  -- event = 'VeryLazy',
+  -- dev = true,
+  opts = function()
+    local palette = require('tokyonight.colors.' .. vim.g.colors_name:gsub('tokyonight%-', ''))
+
+    if palette == nil then return nil end
+
+    local opts = {
+      i = {
+        fg = palette.green,
+        -- fg = palette.mantle,
+        bold = true,
+      },
+      n = {
+        fg = palette.orange,
+        -- fg = palette.mantle,
+        bold = true,
+      },
+      R = {
+        fg = palette.red,
+        -- fg = palette.mantle,
+        bold = true,
+      },
+      v = {
+        fg = palette.purple,
+        -- fg = palette.mantle,
+        bold = true,
+      },
+      V = {
+        fg = palette.purple,
+        -- fg = palette.mantle,
+        bold = true,
+      },
+      c = {
+        fg = palette.yellow,
+        -- fg = palette.mantle,
+        bold = true,
+      },
+    }
+    return opts
+  end,
+}
