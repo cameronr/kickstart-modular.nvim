7b6395e1ba181a28feba6576e95b1eb13ac536ca
Cameron Ring <cameron@cs.stanford.edu>
2024-10-14 14:38:47 -0700

    fix(line-number-mode-change): fix command color

--- lua/custom/plugins/line-number-mode-change.lua
+++ lua/custom/plugins/line-number-mode-change.lua
@@ -1,41 +1,36 @@
 return {
-  'cameronr/line-number-change-mode.nvim',
-  -- event = 'VeryLazy',
-  -- dev = true,
+  'sethen/line-number-change-mode.nvim',
   opts = function()
     local palette = require('tokyonight.colors').setup()
     if palette == nil then return nil end
 
     local opts = {
-      i = {
-        fg = palette.green,
-        -- fg = palette.mantle,
-        bold = true,
-      },
-      n = {
-        fg = palette.orange,
-        -- fg = palette.mantle,
-        bold = true,
-      },
-      R = {
-        fg = palette.red,
-        -- fg = palette.mantle,
-        bold = true,
-      },
-      v = {
-        fg = palette.purple,
-        -- fg = palette.mantle,
-        bold = true,
-      },
-      V = {
-        fg = palette.purple,
-        -- fg = palette.mantle,
-        bold = true,
-      },
-      c = {
-        fg = palette.yellow,
-        -- fg = palette.mantle,
-        bold = true,
+      mode = {
+        i = {
+          fg = palette.green,
+          bold = true,
+        },
+        n = {
+          fg = palette.orange,
+          -- fg = palette.blue,
+          bold = true,
+        },
+        R = {
+          fg = palette.red,
+          bold = true,
+        },
+        v = {
+          fg = palette.purple,
+          bold = true,
+        },
+        V = {
+          fg = palette.purple,
+          bold = true,
+        },
+        c = {
+          fg = palette.yellow,
+          bold = true,
+        },
       },
     }
     return opts
