d19784a34ac48c024e94d5667518570b24165f9b
Cameron Ring <cameron@cs.stanford.edu>
2024-10-17 23:48:07 -0700

    feat(harpoon): keymaps for quick setting

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,55 +1,49 @@
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
-  keys = {
-    {
-      '<leader>oo',
-      function()
-        local harpoon = require('harpoon')
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end,
-      desc = 'Harpoon open',
-    },
-    {
-      '<leader>oa',
-      function() require('harpoon'):list():add() end,
-      desc = 'Harpoon file',
-    },
-    {
-      '<leader>or',
-      function() require('harpoon'):list():remove() end,
-      desc = 'Unharpoon file',
-    },
-    {
-      '<leader>oc',
-      function() require('harpoon'):list():clear() end,
-      desc = 'Unharpoon all files',
-    },
-    {
-      '<leader>1',
-      function() require('harpoon'):list():select(1) end,
-      desc = 'Harpoon 1',
-    },
-    {
-      '<leader>2',
-      function() require('harpoon'):list():select(2) end,
-      desc = 'Harpoon 2',
-    },
-    {
-      '<leader>3',
-      function() require('harpoon'):list():select(3) end,
-      desc = 'Harpoon 3',
-    },
-    {
-      '<leader>4',
-      function() require('harpoon'):list():select(4) end,
-      desc = 'Harpoon 4',
-    },
-    {
-      '<leader>5',
-      function() require('harpoon'):list():select(5) end,
-      desc = 'Harpoon 5',
-    },
-  },
+  keys = function()
+    local keys = {
+      {
+        '<leader>oo',
+        function()
+          local harpoon = require('harpoon')
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = 'Harpoon open',
+      },
+      {
+        '<leader>oa',
+        function() require('harpoon'):list():add() end,
+        desc = 'Harpoon file',
+      },
+      {
+        '<leader>or',
+        function() require('harpoon'):list():remove() end,
+        desc = 'Unharpoon file',
+      },
+      {
+        '<leader>oc',
+        function() require('harpoon'):list():clear() end,
+        desc = 'Unharpoon all files',
+      },
+    }
+
+    for i = 1, 5 do
+      table.insert(keys, {
+        '<leader>' .. i,
+        function() require('harpoon'):list():select(i) end,
+        desc = 'Harpoon ' .. i,
+      })
+
+      table.insert(keys, {
+        '<leader>o' .. i,
+        function() require('harpoon'):list():replace_at(i) end,
+        desc = 'Set buffer as ' .. i,
+      })
+    end
+
+    return keys
+  end,
+
   opts = {},
 }
