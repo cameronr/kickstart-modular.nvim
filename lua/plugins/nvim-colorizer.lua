c88851a4028560e9e3fe0bcac43ec2a5949a8775
Cameron Ring <cameron@cs.stanford.edu>
2024-10-24 18:16:57 -0700

    fix(nvim-colorizer): switch to my fork with fixes
    Also, disable highlighter in some filetypes

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,11 +1,20 @@
 return {
-  'NvChad/nvim-colorizer.lua',
+  'cameronr/nvim-colorizer.lua',
+  -- dev = true,
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     user_default_options = {
       virtualtext = '⬤ ',
       mode = 'virtualtext',
       names = false,
+    },
+    filetypes = {
+      '*',
+      '!cmp_menu',
+      '!fidget',
+      '!notify',
+      '!TelescopeResults',
+      '!TelescopePrompt',
     },
   },
 }
