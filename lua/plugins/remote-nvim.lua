af81572512c651289288431d35e06e7f01ff05c0
Cameron Ring <cameron@cs.stanford.edu>
2024-10-27 10:34:45 -0700

    feat(remote-nvim): trying it out

---
+++ lua/plugins/remote-nvim.lua
@@ -0,0 +1,6 @@
+return {
+  'amitds1997/remote-nvim.nvim',
+  version = '*', -- Pin to GitHub releases
+  cmd = { 'RemoteStart', 'RemoteStop', 'RemoteInfo', 'RemoteCleanup', 'RemoteConfigDel', 'RemoteLog' },
+  config = true,
+}
