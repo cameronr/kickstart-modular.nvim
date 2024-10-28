7a14605b866e6c05f2cb67f5a12ad94d6ec244df
Cameron Ring <cameron@cs.stanford.edu>
2024-10-28 14:15:20 -0700

    fix(ftplugin/lua): set buf relative keymaps

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,3 @@
 -- Testing
-vim.keymap.set('n', '<leader>ct', '<cmd>PlenaryBustedFile %<CR>', { desc = 'Test file' })
-vim.keymap.set('n', '<leader>cT', '<cmd>PlenaryBustedFile %<CR>', { desc = 'Test directory' })
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>ct', '<cmd>PlenaryBustedFile %<CR>', { desc = 'Test file' })
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>cT', '<cmd>PlenaryBustedFile %<CR>', { desc = 'Test directory' })
