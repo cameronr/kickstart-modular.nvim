4169286a5fbde1cd0457d5f4119c78d0632e31b2
Cameron Ring <cameron@cs.stanford.edu>
2024-11-02 10:09:15 -0700

    fix(debugprint): keymap descriptions
    Gives which-key descriptions when plugin hasn't loaded yet

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -1,7 +1,10 @@
 return {
   'andrewferrier/debugprint.nvim',
   keys = {
-    { 'g?' },
+    { 'g?p', desc = 'Plain debug below current line' },
+    { 'g?P', desc = 'Plain debug above current line' },
+    { 'g?v', desc = 'Variable debug below current line' },
+    { 'g?V', desc = 'Variable debug above current line' },
   },
   cmd = { 'DeleteDebugPrints', 'ToggleCommentDebugPrints' },
   opts = {},
