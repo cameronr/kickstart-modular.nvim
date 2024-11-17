382c79a8efa7492768e0486137ad47d1393a149c
Cameron Ring <cameron@cs.stanford.edu>
2024-11-16 18:58:02 -0800

    fix(mini.ai): disable default for %
    It's handled by matchup so don't want the default mini.ai behavior

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -21,6 +21,7 @@ return {
       ai.setup({
         n_lines = 500,
         custom_textobjects = {
+          ['%'] = '',
           s = ai.gen_spec.treesitter({ -- code block
             a = { '@block.outer', '@conditional.outer', '@loop.outer' },
             i = { '@block.inner', '@conditional.inner', '@loop.inner' },
