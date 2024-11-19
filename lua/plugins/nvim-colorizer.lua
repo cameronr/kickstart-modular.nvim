c3075ac579bc2220121dd25f064c3f1e97e7f8f1
Cameron Ring <cameron@cs.stanford.edu>
2024-11-19 10:18:53 -0800

    fix(colorizer): upstream is fixed

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -1,13 +1,29 @@
 return {
-  'cameronr/nvim-colorizer.lua',
+  'NvChad/nvim-colorizer.lua',
   -- dev = true,
   event = { 'BufReadPre', 'BufNewFile' },
   opts = {
     user_default_options = {
-      virtualtext = '⬤ ',
+      RGB = true,
+      RRGGBB = true,
+      names = true,
+      RRGGBBAA = true,
+      AARRGGBB = true,
+      rgb_fn = true,
+      hsl_fn = true,
+      css = true,
+      css_fn = true,
+      -- mode = 'background',
       mode = 'virtualtext',
-      names = false,
+      tailwind = 'lsp',
+      sass = {
+        enable = false,
+        parsers = { 'css' },
+      },
+      virtualtext = '⬤ ',
     },
+    buftypes = { '!prompt', '!popup' },
+
     filetypes = {
       '*',
       '!cmp_menu',
