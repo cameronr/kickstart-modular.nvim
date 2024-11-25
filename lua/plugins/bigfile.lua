734d1364ad91115393eaa7c7732352f0ea1bf0d7
Cameron Ring <cameron@cs.stanford.edu>
2024-11-24 21:48:41 -0500

    fix(bigfile): have to get line count externally

--- lua/plugins/bigfile.lua
+++ lua/plugins/bigfile.lua
@@ -5,14 +5,21 @@ return {
   opts = {
     filesize = 1,
     pattern = function(bufnr, _)
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-      local line_count = vim.api.nvim_buf_line_count(bufnr)
+      local buf_name = vim.api.nvim_buf_get_name(bufnr)
+      ---@diagnostic disable-next-line: undefined-field
+      local ok, stats = pcall(vim.loop.fs_stat, buf_name)
+      -- local line_count = vim.api.nvim_buf_line_count(bufnr)
+      if not ok or not stats then return false end
 
-      if ok and stats then
-        if stats.size > 1024 * 512 then
-          vim.notify('Bigfile detected')
-          return true
-        elseif (stats.size > (10 * 1024)) and (stats.size / line_count) > 250 then
+      if stats.size > 1024 * 512 then
+        vim.notify('Bigfile detected')
+        return true
+      else
+        local cmd = string.format("wc -l '%s'", buf_name)
+        local output = vim.fn.system(cmd)
+        local line_count = tonumber(output:match('^%s*(%d+)'))
+
+        if (stats.size > (10 * 1024)) and (stats.size / line_count) > 250 then
           vim.notify('Long lines detected, bytes: ' .. stats.size .. ', lines: ' .. line_count .. ', bytes / lines: ' .. math.floor(stats.size / line_count))
           vim.b[bufnr].trouble_lualine = false
           return true
