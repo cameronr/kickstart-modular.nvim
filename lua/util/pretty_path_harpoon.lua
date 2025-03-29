1653d8f1835b2718b71a239e74de7899b4c4807a
Cameron Ring <cameron@cs.stanford.edu>
2025-03-29 09:23:26 -0700

    fix(pretty_path_harpoon): for non cwd rel paths

--- lua/util/pretty_path_harpoon.lua
+++ lua/util/pretty_path_harpoon.lua
@@ -3,7 +3,7 @@
 local M = require('lualine-pretty-path.providers.base'):extend()
 
 function M:render_symbols()
-  local harpoonItem, harpoonIndex = require('harpoon'):list():get_by_value(vim.fn.bufname('%'))
+  local harpoonItem, harpoonIndex = require('harpoon'):list():get_by_value(vim.fn.fnamemodify(vim.fn.bufname('%'), ':.'))
   if harpoonItem then
     local harpoonIndexChar = ({ '¹', '²', '³', '⁴', '⁵' })[harpoonIndex]
     -- local harpoonIndexChar = ' '..({ '1', '2', '3', '4', '5' })[harpoonIndex]..'↾'
