54eb2c26d3020d1e28e8bd98a18c5b5794291901
Cameron Ring <cameron@cs.stanford.edu>
2025-04-12 13:44:19 -0700

    feat(tiny-inline-diagnostics): trying out
    Native virtual text doesn't wrap

---
+++ lua/plugins/tiny-inline-diagnostics.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    enabled = vim.fn.has('nvim-0.10') == 1,
+    event = 'VeryLazy',
+    priority = 1000,
+    opts = function()
+      vim.diagnostic.config({ virtual_text = false, float = false })
+      return {
+        signs = {
+          diag = '●',
+          vertical = ' │',
+          vertical_end = ' └',
+          left = '',
+          right = '',
+        },
+        blend = {
+          factor = 0.1,
+        },
+      }
+    end,
+  },
+}
