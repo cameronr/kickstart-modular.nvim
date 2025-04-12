d5bb1968235b65156044b532d0ccb5a1bf0b2995
Cameron Ring <cameron@cs.stanford.edu>
2025-04-12 13:42:27 -0700

    fix(markdown-preview): only load manually

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,5 +3,4 @@ return {
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   build = 'cd app && yarn install',
   init = function() vim.g.mkdp_filetypes = { 'markdown' } end,
-  ft = { 'markdown' },
 }
