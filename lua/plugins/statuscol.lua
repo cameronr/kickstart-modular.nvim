28b7354bd17d2b0bd35407f8b8d4ab8299866de1
Cameron Ring <cameron@cs.stanford.edu>
2025-05-11 14:47:46 -0700

    fix(statuscol): fix widths, enable click handlers

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -10,8 +10,27 @@ return {
       { 'yaocccc/vim-showmarks' },
     },
 
-    opts = {
-      relculright = true,
-    },
+    opts = function()
+      local builtin = require('statuscol.builtin')
+      return {
+        relculright = true,
+        segments = {
+          {
+            sign = {
+              text = { '.*' }, -- to capture diagnostics and gitsigns
+              name = { '.*' }, -- to capture todo-comments signs, marks
+              maxwidth = 1,
+              colwidth = 1,
+            },
+            click = 'v:lua.ScSa',
+          },
+          {
+            text = { builtin.lnumfunc, ' ' },
+            condition = { true, builtin.not_empty },
+            -- click = 'v:lua.ScLa', -- NOTE: uncomment for DAP breakpoint toggling
+          },
+        },
+      }
+    end,
   },
 }
